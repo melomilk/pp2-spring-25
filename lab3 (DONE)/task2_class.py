1f1ae169b49c729c95b75a75b4d6fa3c344df11c
Milana <pakmv2006@gmail.com>
2025-02-05 18:26:39 +0500

    task 2

--- lab3 (DONE)/task2_class.py
+++ lab3 (DONE)/task2_class.py
@@ -3,11 +3,11 @@ class Shape:
         return 0
 
 class Square(Shape):
-    def __init__(self, length):
-        self.length = length
+    def __init__(self):
+        self.length=float(input("Length: "))
 
     def area(self):
         return self.length ** 2
 
-square = Square(5)
+square = Square()
 print(square.area())  
