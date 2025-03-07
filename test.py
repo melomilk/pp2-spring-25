1ad0360c01ece0e5d629deab2f03743ea2c12d5a
Milana <pakmv2006@gmail.com>
2025-03-07 14:53:48 +0500

    task3

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 import os
 path = os.getcwd()
-print(f"Path accessibility: Existance: {os.access(path, mode=os.EX_OK)}, Reading: {os.access(path, mode=os.R_OK)}, Writing: {os.access(path, mode=os.W_OK)}, Executability: {os.access(path, mode=os.X_OK)}")
+def checkUp(path):
+    if os.access(path, mode=os.EX_OK):
+        return os.listdir(path)
+    else:
+        return False
+print(checkUp(path))
