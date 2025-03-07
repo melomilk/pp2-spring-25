e91caa7fbb7b61b4042df995b007c764cddede57
Milana <pakmv2006@gmail.com>
2025-03-07 14:48:34 +0500

    upperlower

--- lab6/builtinfunc2.py
+++ lab6/builtinfunc2.py
@@ -1,4 +1,4 @@
-string = 'iLoVeDaNa'
+string = 'iLoVeCaKeS'
 upper = sum(map(lambda x: x.isupper(), string))
 lower = sum(map(lambda x: x.islower(), string))
 print(f"Upper: {upper}, lower: {lower}")
